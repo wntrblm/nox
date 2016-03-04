4245084216395b6f77954abdd23b483b06fbf626
Jon Wayne Parrott <jjramone13@gmail.com>
2016-03-03 17:44:09 -0800

    Relaxing dependency version ranges. Fixes #5 and allows installation on system python in os x.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
 
     install_requires=[
         'colorlog>=2.6.1,<3.0.0',
-        'py>=1.4.31,<2.0.0',
+        'py>=1.4.0,<2.0.0',
         'six>=1.4.0,<2.0.0',
         'virtualenv>=14.0.0'],
 
