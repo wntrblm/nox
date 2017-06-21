23f56de1ef4cbfe3723465e0d49bd9bb92abd4c1
Jon Wayne Parrott <jjramone13@gmail.com>
2017-06-20 21:24:43 -0700

    Release v0.17.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = open('README.rst', 'r', encoding='utf-8').read()
 setup(
     name='nox-automation',
 
-    version='0.16.1',
+    version='0.17.0',
 
     description='Flexible test automation.',
     long_description=long_description,
