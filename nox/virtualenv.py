936411b652d143704b067b253e247772b4bd4edd
Paul Moore <p.f.moore@gmail.com>
2017-09-20 11:40:45 +0100

    Fix flake8 issues

--- nox/virtualenv.py
+++ nox/virtualenv.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import distutils.spawn
 import os
 import platform
 import re
 import shutil
-import subprocess
 import sys
 
 import py
