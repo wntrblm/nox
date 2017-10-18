6cebecd1915c3eb584f7f79b2f1ae7d324131e8c
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-18 14:05:19 -0700

    Revert "Tweak that won't change anything."
    This reverts commit a3fb7e975c9b495b027e59b817b566f64a076211.

--- appveyor/install_python363.ps1
+++ appveyor/install_python363.ps1
@@ -10,13 +10,14 @@ $py_url = "https://www.python.org/ftp/python"
 # First do 32-bit.
 $exe_suffix=""
 $target_dir="C:\\Python36"
+$launcher=0
 Write-Host "Installing Python 3.6.3$exe_suffix..." -ForegroundColor Cyan
 $exePath = "$env:TEMP\python-3.6.3${exe_suffix}.exe"
 $downloadFile = "$py_url/3.6.3/python-3.6.3${exe_suffix}.exe"
 Write-Host "Downloading $downloadFile..."
 (New-Object Net.WebClient).DownloadFile($downloadFile, $exePath)
 Write-Host "Installing..."
-cmd /c start /wait $exePath /passive InstallAllUsers=1 TargetDir="$target_dir" Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0
+cmd /c start /wait $exePath /passive InstallAllUsers=1 TargetDir="$target_dir" Shortcuts=0 Include_launcher=$launcher InstallLauncherAllUsers=$launcher
 Write-Host "Python 3.6.3 installed to $target_dir"
 
 echo "$(& $target_dir\python.exe --version 2> $null)"
@@ -24,13 +25,14 @@ echo "$(& $target_dir\python.exe --version 2> $null)"
 # Then do 64-bit.
 $exe_suffix="-amd64"
 $target_dir="C:\\Python36-x64"
+$launcher=1
 Write-Host "Installing Python 3.6.3$exe_suffix..." -ForegroundColor Cyan
 $exePath = "$env:TEMP\python-3.6.3${exe_suffix}.exe"
 $downloadFile = "$py_url/3.6.3/python-3.6.3${exe_suffix}.exe"
 Write-Host "Downloading $downloadFile..."
 (New-Object Net.WebClient).DownloadFile($downloadFile, $exePath)
 Write-Host "Installing..."
-cmd /c start /wait $exePath /passive InstallAllUsers=1 TargetDir="$target_dir" Shortcuts=0 Include_launcher=1 InstallLauncherAllUsers=1
+cmd /c start /wait $exePath /passive InstallAllUsers=1 TargetDir="$target_dir" Shortcuts=0 Include_launcher=$launcher InstallLauncherAllUsers=$launcher
 Write-Host "Python 3.6.3 installed to $target_dir"
 
 echo "$(& $target_dir\python.exe --version 2> $null)"
