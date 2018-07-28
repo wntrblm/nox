ad56baafab381ef967f504b1c2d9dc30d61bc96e
Thea Flowers <theaflowers@google.com>
2018-07-28 00:23:35 -0700

    Add missing attrs dependency

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ setup(
         'colorlog>=2.6.1,<3.0.0',
         'py>=1.4.0,<2.0.0',
         'six>=1.4.0,<2.0.0',
-        'virtualenv>=14.0.0'],
+        'virtualenv>=14.0.0',
+        'attrs'],
 
     extras_require={
         ':python_version<"3.4"': ['enum34'],
