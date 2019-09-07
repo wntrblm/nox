f9da3e2ff36057784991fd90b31234cec4c65d8c
Chad Smith <cs01@users.noreply.github.com>
2019-09-07 09:35:47 -0700

    fix lint

--- tests/test__option_set.py
+++ tests/test__option_set.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-from unittest import mock
-
 import pytest
 from nox import _option_set, _options
 
