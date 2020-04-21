2f86a1bd31b12dc5e896094cb6dbfb8767b8cf9d
Sylvain MARIE <sylvain.marie@se.com>
2020-04-21 15:17:27 +0200

    Last coverage fix ?

--- nox/virtualenv.py
+++ nox/virtualenv.py
@@ -169,7 +169,7 @@ def _is_connected(hostname: str = None, url: str = None) -> bool:
             # HTTP resolution, compliant with http proxy env settings
             code = requests.get(url).status_code
             assert code is not None
-    except:
+    except:  # pragma: no cover
         return False
     else:
         return True
