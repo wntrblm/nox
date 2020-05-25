82ba5b34715784df11767a1df32cf0cab8a3faae
Thea Flowers <thea@thea.codes>
2020-05-24 20:24:41 -0700

    Release 2020.5.24

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = open("README.rst", "r", encoding="utf-8").read()
 
 setup(
     name="nox",
-    version="2019.11.9",
+    version="2019.5.24",
     description="Flexible test automation.",
     long_description=long_description,
     url="https://nox.thea.codes",
