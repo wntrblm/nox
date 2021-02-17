b768b1db3ee863d04a06dbccda05df101aa30977
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-17 10:17:41 +0100

    feat(_version): Add _{parse,read}_needs_version()

--- nox/_version.py
+++ nox/_version.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import ast
+import sys
+from typing import Optional
+
 try:
     import importlib.metadata as metadata
 except ImportError:  # pragma: no cover
@@ -21,3 +25,38 @@ except ImportError:  # pragma: no cover
 def get_nox_version() -> str:
     """Return the version of the installed Nox package."""
     return metadata.version("nox")
+
+
+def _parse_string_constant(node: ast.AST) -> Optional[str]:
+    """Return the value of a string constant."""
+    if sys.version_info < (3, 8):  # pragma: no cover
+        if isinstance(node, ast.Str) and isinstance(node.s, str):
+            return node.s
+    elif isinstance(node, ast.Constant) and isinstance(node.value, str):
+        return node.value
+    return None  # pragma: no cover
+
+
+def _parse_needs_version(source: str, filename: str = "<unknown>") -> Optional[str]:
+    """Parse ``nox.needs_version`` from the user's noxfile."""
+    value: Optional[str] = None
+    module: ast.Module = ast.parse(source, filename=filename)
+    for statement in module.body:
+        if isinstance(statement, ast.Assign):
+            for target in statement.targets:
+                if (
+                    isinstance(target, ast.Attribute)
+                    and isinstance(target.value, ast.Name)
+                    and target.value.id == "nox"
+                    and target.attr == "needs_version"
+                ):
+                    value = _parse_string_constant(statement.value)
+    return value
+
+
+def _read_needs_version(filename: str) -> Optional[str]:
+    """Read ``nox.needs_version`` from the user's noxfile."""
+    with open(filename) as io:
+        source = io.read()
+
+    return _parse_needs_version(source, filename=filename)
