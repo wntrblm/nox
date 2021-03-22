885169149b3647523cd032c6234a476a4bf20db8
Gwyn <gwyn@playtechnique.io>
2021-03-22 15:35:01 -0600

    Adds tracking of whether venv has been create (#394)
    This boolean informs us whether we need to ignore the "no-install"
    command line option.

--- nox/virtualenv.py
+++ nox/virtualenv.py
@@ -55,6 +55,8 @@ class ProcessEnv:
         self._bin_paths = bin_paths
         self.env = os.environ.copy()
 
+        self._venv_created = False
+
         if env is not None:
             self.env.update(env)
 
@@ -77,6 +79,11 @@ class ProcessEnv:
         if paths is None:
             raise ValueError("The environment does not have a bin directory.")
         return paths[0]
+
+    @property
+    def venv_created(self):
+        return self._venv_created
+    
 
     def create(self) -> bool:
         raise NotImplementedError("ProcessEnv.create should be overwritten in subclass")
@@ -249,6 +256,8 @@ class CondaEnv(ProcessEnv):
         )
         nox.command.run(cmd, silent=True, log=False)
 
+        self.venv_created = True
+
         return True
 
     @staticmethod
@@ -418,4 +427,6 @@ class VirtualEnv(ProcessEnv):
         )
         nox.command.run(cmd, silent=True, log=False)
 
+        self._venv_created = True
+        
         return True
