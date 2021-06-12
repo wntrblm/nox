51016f9a323b8247167bd35aa27f2058d2144e27
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-12 23:21:01 +0200

    Release 2021.6.12

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = open("README.rst", "r", encoding="utf-8").read()
 
 setup(
     name="nox",
-    version="2021.6.6",
+    version="2021.6.12",
     description="Flexible test automation.",
     long_description=long_description,
     url="https://nox.thea.codes",
