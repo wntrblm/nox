c459e48f5e11737a64a6da66d6d3d9c171ffa994
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-01-12 10:38:25 -0500

    docs: use myst-parser for md (#561)

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ sys.path.insert(0, os.path.abspath("."))
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "recommonmark",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
