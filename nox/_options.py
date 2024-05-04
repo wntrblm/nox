a49f076ae854b3ad512d78ac53bf2ea8153c6bc5
Fred Stober <mail@fredstober.de>
2024-05-04 14:49:28 +0200

    feat: add 'max_log_args' option that allows to limit the number of arguments that are shown in the logging output

--- nox/_options.py
+++ nox/_options.py
@@ -415,6 +415,16 @@ options.add_options(
         noxfile=True,
     ),
     _option_set.Option(
+        "max_log_args",
+        "-ma",
+        "--max-log-args",
+        group=options.groups["reporting"],
+        default=None,
+        type=int,
+        help="Set a limit on the maximum number of arguments that are logged.",
+        noxfile=True,
+    ),
+    _option_set.Option(
         "default_venv_backend",
         "-db",
         "--default-venv-backend",
