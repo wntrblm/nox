12b805ac1e4db2ee6bd17061cfbf0a2535662389
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-01-14 21:38:07 -0500

    feat: drop support for tox 3 (#910)
    * feat: drop support for tox 3
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * Update ci.yml
    
    * tests: include Python version
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * fix: drop unused code
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- nox/tox_to_nox.py
+++ nox/tox_to_nox.py
@@ -21,13 +21,11 @@ import os
 import re
 import sys
 from configparser import ConfigParser
-from importlib import metadata
 from pathlib import Path
 from subprocess import check_output
 from typing import Any, Iterable
 
 import jinja2
-import tox.config
 
 if sys.version_info < (3, 9):
     from importlib_resources import files
@@ -42,22 +40,12 @@ def __dir__() -> list[str]:
     return __all__
 
 
-TOX_VERSION = metadata.version("tox")
-
-TOX4 = int(TOX_VERSION.split(".")[0]) >= 4
-
 DATA = files("nox")
 
-if TOX4:
-    _TEMPLATE = jinja2.Template(
-        DATA.joinpath("tox4_to_nox.jinja2").read_text(encoding="utf-8"),
-        extensions=["jinja2.ext.do"],
-    )
-else:
-    _TEMPLATE = jinja2.Template(
-        DATA.joinpath("tox_to_nox.jinja2").read_text(encoding="utf-8"),
-        extensions=["jinja2.ext.do"],
-    )
+_TEMPLATE = jinja2.Template(
+    DATA.joinpath("tox4_to_nox.jinja2").read_text(encoding="utf-8"),
+    extensions=["jinja2.ext.do"],
+)
 
 
 def wrapjoin(seq: Iterable[Any]) -> str:
@@ -66,14 +54,11 @@ def wrapjoin(seq: Iterable[Any]) -> str:
 
 
 def fixname(envname: str) -> str:
-    """Replace dashes with underscores and check if the result is a valid identifier."""
-    envname = envname.replace("-", "_").replace("testenv:", "")
-    if not envname.isidentifier():
-        print(
-            f"Environment {envname!r} is not a valid nox session name.\n"
-            "Manually update the session name in noxfile.py before running nox."
-        )
-    return envname
+    """
+    Replace dashes with underscores. Tox 4+ requires valid identifiers for
+    names already.
+    """
+    return envname.replace("-", "_").replace("testenv:", "")
 
 
 def write_output_to_file(output: str, filename: str) -> None:
@@ -88,65 +73,60 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    if TOX4:
-        output = check_output(["tox", "config"], text=True, encoding="utf-8")  # noqa: S607
-        original_config = ConfigParser()
-        original_config.read_string(output)
-        config: dict[str, dict[str, Any]] = {}
+    output = check_output(["tox", "config"], text=True, encoding="utf-8")  # noqa: S607
+    original_config = ConfigParser()
+    original_config.read_string(output)
+    config: dict[str, dict[str, Any]] = {}
 
-        for name, section in original_config.items():
-            if name == "DEFAULT":
-                continue
+    for name, section in original_config.items():
+        if name == "DEFAULT":
+            continue
 
-            config[name] = dict(section)
-            # Convert set_env from string to dict
-            set_env = {}
-            for var in section.get("set_env", "").strip().splitlines():
-                k, v = var.split("=")
-                if k not in {
-                    "PYTHONHASHSEED",
-                    "PIP_DISABLE_PIP_VERSION_CHECK",
-                    "PYTHONIOENCODING",
-                }:
-                    set_env[k] = v
+        config[name] = dict(section)
+        # Convert set_env from string to dict
+        set_env = {}
+        for var in section.get("set_env", "").strip().splitlines():
+            k, v = var.split("=")
+            if k not in {
+                "PYTHONHASHSEED",
+                "PIP_DISABLE_PIP_VERSION_CHECK",
+                "PYTHONIOENCODING",
+            }:
+                set_env[k] = v
 
-            config[name]["set_env"] = set_env
+        config[name]["set_env"] = set_env
 
-            config[name]["commands"] = [
-                wrapjoin(c.split()) for c in section["commands"].strip().splitlines()
+        config[name]["commands"] = [
+            wrapjoin(c.split()) for c in section["commands"].strip().splitlines()
+        ]
+
+        config[name]["deps"] = wrapjoin(
+            [
+                part
+                for dep in section["deps"].strip().splitlines()
+                for part in (dep.split() if dep.startswith("-r") else [dep])
             ]
+        )
 
-            config[name]["deps"] = wrapjoin([
-                part for dep
-                in section["deps"].strip().splitlines()
-                for part in
-                (dep.split() if dep.startswith("-r") else [dep])
-            ])
+        for option in "skip_install", "use_develop":
+            if section.get(option):
+                if section[option] == "False":
+                    config[name][option] = False
+                else:
+                    config[name][option] = True
 
-            for option in "skip_install", "use_develop":
-                if section.get(option):
-                    if section[option] == "False":
-                        config[name][option] = False
-                    else:
-                        config[name][option] = True
+        if os.path.isabs(section["base_python"]) or re.match(
+            r"py\d+", section["base_python"]
+        ):
+            impl = "python" if section["py_impl"] == "cpython" else section["py_impl"]
+            config[name]["base_python"] = impl + section["py_dot_ver"]
 
-            if os.path.isabs(section["base_python"]) or re.match(
-                r"py\d+", section["base_python"]
-            ):
-                impl = (
-                    "python" if section["py_impl"] == "cpython" else section["py_impl"]
-                )
-                config[name]["base_python"] = impl + section["py_dot_ver"]
-
-            change_dir = Path(section.get("change_dir"))
-            rel_to_cwd = change_dir.relative_to(Path.cwd())
-            if str(rel_to_cwd) == ".":
-                config[name]["change_dir"] = None
-            else:
-                config[name]["change_dir"] = rel_to_cwd
-
-    else:
-        config = tox.config.parseconfig([])
+        change_dir = Path(section.get("change_dir"))
+        rel_to_cwd = change_dir.relative_to(Path.cwd())
+        if str(rel_to_cwd) == ".":
+            config[name]["change_dir"] = None
+        else:
+            config[name]["change_dir"] = rel_to_cwd
 
     output = _TEMPLATE.render(config=config, wrapjoin=wrapjoin, fixname=fixname)
 
