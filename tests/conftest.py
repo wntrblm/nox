53bf1910164a286212dd9349f6c1bbefb2dca1c2
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-10-13 18:53:40 -0400

    tests: add conda marker (#969)
    * tests: add conda marker
    * Update tests/conftest.py
    
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,11 +14,15 @@
 from __future__ import annotations
 
 import re
+import shutil
+import subprocess
 from pathlib import Path
 from string import Template
-from typing import Callable
+from typing import Any, Callable
 
 import pytest
+
+HAS_CONDA = shutil.which("conda") is not None
 
 
 @pytest.fixture(autouse=True)
@@ -58,3 +62,33 @@ def generate_noxfile_options(tmp_path: Path) -> Callable[..., str]:
         return str(path)
 
     return generate_noxfile
+
+
+# This fixture will be automatically used unless the test has the 'conda' marker
+@pytest.fixture
+def prevent_conda(monkeypatch: pytest.MonkeyPatch) -> None:
+    def blocked_popen(*args: Any, **kwargs: Any) -> Any:
+        cmd = args[0][0] if isinstance(args[0], list) else args[0]
+        msg = "Use of 'conda' command is blocked in tests without @pytest.mark.conda"
+        if "conda" in cmd or "mamba" in cmd:
+            raise RuntimeError(msg)
+        return original_popen(*args, **kwargs)
+
+    original_popen = subprocess.Popen
+    monkeypatch.setattr(subprocess, "Popen", blocked_popen)
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
+    for item in items:
+        if "make_conda" in getattr(item, "fixturenames", ()):
+            item.add_marker("conda")
+        if "conda" in item.keywords:
+            item.add_marker(
+                pytest.mark.skipif(not HAS_CONDA, reason="Missing conda command.")
+            )
+
+
+# Protection to make sure every conda-using test requests it
+def pytest_runtest_setup(item: pytest.Item) -> None:
+    if not any(mark.name == "conda" for mark in item.iter_markers()):
+        item.add_marker(pytest.mark.usefixtures("prevent_conda"))
