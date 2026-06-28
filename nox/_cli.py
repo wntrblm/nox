b187988541bfd6c2b6b3f0121bff40865046da90
Felix Fontein <felix@fontein.de>
2026-06-28 22:03:03 +0200

    Add a new method Session.run_capture() which allows to capture stdout and stderr separately, even while showing the result.

--- nox/_cli.py
+++ nox/_cli.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 __lazy_modules__ = {
+    "asyncio",
     "importlib",
     "importlib.metadata",
     "nox._option_set",
@@ -37,6 +38,7 @@ __lazy_modules__ = {
     "urllib.parse",
 }
 
+import asyncio
 import importlib.metadata
 import os
 import shutil
@@ -467,6 +469,7 @@ def _main(*, main_ep: bool) -> None:
                 )
 
     nox.registry.reset()
+    asyncio.set_event_loop(asyncio.new_event_loop())
     exit_code = execute_workflow(args)
 
     # Done; exit.
