3df62ddfaa4e41fcf9c09d2bdebbb13272f3422b
Felix Fontein <felix@fontein.de>
2026-07-18 13:14:43 +0200

    Create event loop when needed, and shut it down afterwards.
    This fixes issues with the tests (no more "unclosed event loops"
    errors), and probably allows tee_popen() to run in parallel.

--- nox/popen.py
+++ nox/popen.py
@@ -262,11 +262,10 @@ class _TeeSubprocess:
     def __init__(
         self,
         *,
-        loop: asyncio.AbstractEventLoop,
         interrupt_timeout: float | None,
         terminate_timeout: float | None,
     ) -> None:
-        self.loop = loop
+        self.loop = asyncio.new_event_loop()
         self.stdout: list[bytes] = []
         self.stderr: list[bytes] = []
         self.interrupt_timeout = interrupt_timeout
@@ -454,6 +453,7 @@ class _TeeSubprocess:
             )
         finally:
             self.loop.remove_signal_handler(signal.SIGINT)
+            self.loop.close()
         sys.stdout.flush()
         if is_canceled:
             raise KeyboardInterrupt
@@ -476,9 +476,7 @@ def tee_popen(
     if sys.platform.startswith("win") and args[0].casefold().endswith((".bat", ".cmd")):
         popen_args = _windows_batch_command(args, env)
 
-    loop = asyncio.get_event_loop()
     teesub = _TeeSubprocess(
-        loop=loop,
         interrupt_timeout=interrupt_timeout,
         terminate_timeout=terminate_timeout,
     )
